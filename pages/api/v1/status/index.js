4867847a85113bcd3b0a11c8490ad7603cee3d30
rafagurila11@gmail.com <rafagurila11@gmail.com>
2025-11-11 17:25:25 -0300

    movendo .dot env, e arquivo de desenvolvimento e configuração de var de ambiente

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,14 @@
 // pages/api/status.js - versao 1 do endpoint de status
-function status(request, response) {
+
+import database from "../../../../infra/database.js"; // Importa o módulo de banco de dados
+
+async function status(request, response) {
+  // Função que lida com a requisição de status
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result);
   response
     .status(200)
-    .json({ chave: "função status retorna 200 como já sendo uma rota válida" });
+    .json({ chave: "função status retorna 200 como já sendo uma rota válida" }); // Retorna status 200 com uma mensagem JSON
 }
 
-export default status;
+export default status; // Exporta a função como padrão para ser usada como handler do endpoint
